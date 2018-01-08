17f2cc58aa440f184c64a1ba04789f6df6b2cb69
capenafi <capenafi@espol.edu.ec>
2018-01-08 14:54:49 -0500

    commit 2

--- main.c
+++ main.c
@@ -4,17 +4,24 @@ int main()
 {
  	FILE *archivo;
  	char caracteres[100];
+	char *cadena;
  	archivo = fopen("prueba.txt","a+");
- 	if (archivo == NULL) exit(1);
+ 	if (archivo == NULL){
+	 exit(1);
+}
  	else
         {
- 	    printf("\nEl contenido del archivo de prueba es \n\n");
+ 	    printf("\nEl contenido del archivo de prueba es: \n\n");
 		while (feof(archivo) == 0)
 		{
 		fgets(caracteres,100,archivo);
 		printf("%s",caracteres);
 		}
-	system("PAUSE");
+		//printf("\nIngrese los caracteres que desea agregar al final del archivo: ");
+		//scanf("%s", cadena);
+		
+		fputs("Agregamos algo.",archivo);
+	//system("PAUSE");
 	}
 	fclose(archivo);
 	return 0;
