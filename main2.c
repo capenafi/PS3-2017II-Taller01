4b455ac1be6314343c902305c2123416dc29366d
capenafi <capenafi@espol.edu.ec>
2018-01-08 21:09:58 -0500

    Primer tema del taller, el codigo se encuentra en main2.c

--- main2.c
+++ main2.c
@@ -3,19 +3,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main()
 {
- 	FILE *archivo;
- 	char caracteres[100];
-	char *cadena;
-	char nombre[100];
 	int fd;
-	printf("Ingrese el nombre del archivo que desea modificar: ");
-	fgets(nombre,100,stdin);
- 	fd=open(nombre,O_RDWR,0);
+	//printf("Ingrese el nombre del archivo que desea modificar: ");
+	//fgets(nombre,100,stdin);
+ 	fd=open("prueba.txt",O_RDWR|O_APPEND|O_CREAT,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
  	if (fd == -1){
 	 exit(1);
+	}else{
+		int error;
+		printf("El descriptor del archivo es: %i \n",fd);
+		error=write(fd,"Carlos Penafiel\n",50);
 	}
-	
+	close(fd);
 }
